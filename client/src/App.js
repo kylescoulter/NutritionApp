4789c0fa47633a62748baa7314730aeb579283b5
kylescoulter <50339406+kylescoulter@users.noreply.github.com>
2020-07-16 18:03:13 -0500

    Created BaseAppHeader. Created Home page. Created Login and LoginContainer. Created Register and Register Container. Added React Router to navigate between pages. Added logout functionality to logout button in Diary Page. Added basic form to RegisterContainer. Added login functionality to Login page. Added register functionality to Login page. Added Routes to App.js.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import {BrowserRouter, Route, Switch, withRouter} from "react-router-dom";
+import Home from "./Home";
 import Diary from "./Diary";
+import Login from "./Login";
+import Register from "./Register";
 
-function App() {
-  return (
-    <div className="App">
-        <Diary/>
-    </div>
-  );
+class App extends Component{
+    render() {
+        return (
+            <BrowserRouter>
+                <div className="App">
+                    <Switch>
+                        <Route exact path='/' component={withRouter(Home)} />
+                        <Route path='/Diary' component={withRouter(Diary)} />
+                        <Route path='/Login' component={withRouter(Login)} />
+                        <Route path='/Register' component={withRouter(Register)} />
+                    </Switch>
+                </div>
+            </BrowserRouter>
+        )
+    }
 }
-
 export default App;
+
--- client/src/Diary.js
+++ client/src/Diary.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import DiaryContainer from "./DiaryContainer";
 import AccountContainer from "./AccountContainer";
 
-function Diary() {
-    return (
-        <div className="Diary">
-            <AccountContainer />
-            <DiaryContainer />
-        </div>
-    );
+class Diary extends Component{
+    render() {
+        return (
+            <div className="Diary">
+                <AccountContainer />
+                <DiaryContainer />
+            </div>
+        );
+    }
 }
-
 export default Diary;
