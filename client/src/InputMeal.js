f6bc2f893926392af96681c4fcd5670e61bb62f3
kylescoulter <50339406+kylescoulter@users.noreply.github.com>
2020-08-12 21:18:46 -0500

    Modified InputMeal css sizing. Added bootstrap basic css styling to index.js. Refactored form in InputMeal to use bootstrap objects. Refactored searchIngredientDatabase to send a list of results to SearchResultModal. Moved buildCard method to SearchResultModal. Modified SearchResultModal to display results of search. Modified sizing of result in searchResults.scss.

--- client/src/InputMeal.js
+++ client/src/InputMeal.js
@@ -1,8 +1,8 @@
 import React from "react"
 import "./App.css"
 import "./searchResults.scss"
-import Button from 'react-bootstrap/Button'
 import SearchResultModal from './SearchResultModal'
+import {InputGroup, Form, Button} from 'react-bootstrap'
 export default class InputMeal extends React.Component {
 
     constructor(props) {
@@ -13,6 +13,7 @@ export default class InputMeal extends React.Component {
             app_id: '1fbd8950',
             app_key: '3f79445dbc8bf58882f1eff5a2d6fb31',
             resultExample: document.querySelector('#result'),
+            resultsList: []
         };
 
         this.handleChange = this.handleChange.bind(this);
@@ -20,6 +21,7 @@ export default class InputMeal extends React.Component {
     }
 
     searchIngredientDatabase = () => {
+
         if (this.state.item !== '') {
             fetch('https://api.edamam.com/api/food-database/v2/parser?ingr=' + this.state.item + '&app_id=1fbd8950&app_key=3f79445dbc8bf58882f1eff5a2d6fb31')
                 .then(response => {
@@ -31,8 +33,10 @@ export default class InputMeal extends React.Component {
                         }
                     })
                 .then((result) => {
+                        let searchResultsList = [];
                         let name = result.parsed[0].food.label;
                         let calories = result.parsed[0].food.nutrients.ENERC_KCAL;
+
                         this.setState({
                             item: name,
                             cal: calories,
@@ -41,9 +45,13 @@ export default class InputMeal extends React.Component {
                         alert("Your ingredient was found: " + name + calories);
                         if (result.hints.length) {
                             result.hints.forEach(hint => {
-                                this.insertCard(hint.food)
+                                //this.insertCard(hint.food)
+                                searchResultsList.push(hint)
                             })
                         }
+                        this.setState( {
+                            resultsList: searchResultsList
+                        });
                     }
                 ).catch((error) => {
                 console.log('error: ' + error);
@@ -51,43 +59,6 @@ export default class InputMeal extends React.Component {
         }
 
     };
-
-    insertCard = (food) => {
-        if (food !== null) {
-            this.state.resultExample.insertAdjacentHTML('beforeend', this.buildCard(food));
-            //this.state.resultExample.insertAdjacentHTML('beforeend', this.displayAddButton(food.label, food.nutrients.ENERC_KCAL));
-        }
-
-    };
-
-    buildCard = (data) => {
-        const energy = data.nutrients.ENERC_KCAL ? `<li><b>Energy: </b><span>${data.nutrients.ENERC_KCAL.toFixed(1)}kcal</span></li>` : '';
-        const carbs = data.nutrients.CHOCDF ? `<li><b>Carbs: </b><span>${data.nutrients.CHOCDF.toFixed(1)}g</span></li>` : '';
-        const protein = data.nutrients.PROCNT ? `<li><b>Protein: </b><span>${data.nutrients.PROCNT.toFixed(1)}g</span></li>` : '';
-        const fat = data.nutrients.FAT ? `<li><b>Fat: </b><span>${data.nutrients.FAT.toFixed(1)}g</span></li>` : 'not';
-
-        return `<div class="card">
-                  <div class="card-header">
-                    <h3>${data.label}</h3>
-                    <h4>${data.category}</h4>
-                  </div>
-                  <div class="card-body">
-                    <ul>
-                      ${energy}
-                      ${carbs}
-                      ${protein}
-                      ${fat}
-                    </ul>
-                  </div>
-                  <div class="card-footer">
-                    <p><b>Brand: </b><span>${data.brand || 'None'}</span></p>
-                    
-                  </div>
-                </div>
-        `
-    };
-
-
 
     addItemToList = (label, energy) => {
         this.setState({
@@ -102,10 +73,6 @@ export default class InputMeal extends React.Component {
         })
     };
 
-    displayAddButton = (label, energy) =>{
-        return <Button onClick={this.addItemToList(label, energy)}>Add</Button>
-    };
-
     handleChange(event) {
         this.setState({
             [event.target.name]: event.target.value
@@ -116,29 +83,40 @@ export default class InputMeal extends React.Component {
         event.preventDefault();
     }
 
-    render() {
 
+    render() {
         return (
             <div className="InputMeal">
-                <form onSubmit={this.handleSubmit}>
-                    <label>Search: </label>
-                    <input
-                        type="text"
-                        placeholder="search for an item/recipe..."
-                        name="item"
-                        onChange={this.handleChange}
-                    />
-                    <button onClick={this.searchIngredientDatabase}>
-                        <img
-                        src={require("./images/search.svg")}
-                        alt="Search" height="10" width="15"
-                        title="Search"
-                        className="Search"
+                <p> </p>
+                <Form onSubmit={this.handleSubmit}>
+                    <InputGroup className="itemSearch" >
+                        <Form.Control
+                            type="text"
+                            placeholder="search for an item!"
+                            name="item"
+                            onChange={this.handleChange}
                         />
-                    </button>
-                    <div id="result">
-                    </div>
-                </form>
+                        <InputGroup.Append>
+                            <Button
+                                variant="outline-secondary"
+                                onClick={this.searchIngredientDatabase}
+                            >
+                                <img
+                                    src={require("./images/search.svg")}
+                                    alt="Search"
+                                    title="Search"
+                                    className="Search"
+
+                                />
+                            </Button>
+                        </InputGroup.Append>
+                    </InputGroup>
+                </Form>
+                <p> </p>
+                <SearchResultModal
+                    searchResults={this.state.resultsList}
+                />
+                <p> </p>
             </div>
 
         )
