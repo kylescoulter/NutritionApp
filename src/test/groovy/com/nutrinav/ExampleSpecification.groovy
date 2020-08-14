aacd1e17d9f623e965daf3789e5604c6667c3ff9
kylescoulter <50339406+kylescoulter@users.noreply.github.com>
2020-08-14 17:17:23 -0500

    Added Spock dependencies. Created ExampleSpecification.

---
+++ src/test/groovy/com/nutrinav/ExampleSpecification.groovy
@@ -0,0 +1,11 @@
+package com.nutrinav
+
+import spock.lang.Specification
+
+
+class ExampleSpecification extends Specification {
+    // fields
+    // fixture methods
+    // feature methods
+    // helper methods
+}
