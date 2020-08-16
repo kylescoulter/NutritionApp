b89306e8bb96a8a385dfc07060667d94d9784553
Kara Kalinowski <mukamuka777@aol.com>
2020-08-16 13:35:44 -0500

    Added tests for creating AccountEntity and IngredientEntity

--- src/test/groovy/com/nutrinav/ExampleSpecification.groovy
+++ src/test/groovy/com/nutrinav/ExampleSpecification.groovy
@@ -1,11 +1,26 @@
 package com.nutrinav
-
+import com.nutrinav.models.*;
 import spock.lang.Specification
 
-
 class ExampleSpecification extends Specification {
-    // fields
-    // fixture methods
-    // feature methods
-    // helper methods
+
+    def ingredientEntity = new IngredientEntity("name", 100);
+    def sampleResponse = new SampleResponse();
+    def "A new AccountEntity is added"() {
+        when:
+        def accountEntity = new AccountEntity("email", "username", "password", "firstName", "lastName");
+        then:
+        accountEntity.email == "email"
+        accountEntity.firstName == "firstName"
+        accountEntity.lastName == "lastName"
+        accountEntity.password == "password"
+        accountEntity.username == "username"
+    }
+    def "A new IngredientEntity is added"() {
+        when:
+        def ingredientEntity = new IngredientEntity("name", 100)
+        then:
+        ingredientEntity.name == "name"
+        ingredientEntity.calories == 100
+    }
 }
