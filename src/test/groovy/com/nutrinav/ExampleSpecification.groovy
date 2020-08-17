a035f637031ffdf141dd33a32e11db3aa4026494
Kara Kalinowski <mukamuka777@aol.com>
2020-08-16 23:56:24 -0500

    Added tests for adding AccountEntity and IngredientEntity to database

--- src/test/groovy/com/nutrinav/ExampleSpecification.groovy
+++ src/test/groovy/com/nutrinav/ExampleSpecification.groovy
@@ -1,14 +1,26 @@
 package com.nutrinav
-import com.nutrinav.models.*;
+
+import com.nutrinav.controllers.*
+import com.nutrinav.models.*
+import org.springframework.context.ApplicationContext
+import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import spock.lang.Specification
+import org.springframework.http.ResponseEntity
+import java.util.Scanner
+
 
 class ExampleSpecification extends Specification {
-
-    def ingredientEntity = new IngredientEntity("name", 100);
-    def sampleResponse = new SampleResponse();
-    def "A new AccountEntity is added"() {
+    ApplicationContext context = new AnnotationConfigApplicationContext("com.nutrinav")
+    AccountController accountController = context.getBean(AccountController.class)
+    AccountEntity accountEntity
+    ResponseEntity<AccountEntity> accountEntityResponse
+    IngredientController ingredientController = context.getBean(IngredientController.class)
+    IngredientEntity ingredientEntity
+    ResponseEntity<IngredientEntity> ingredientEntityResponse
+    def sampleResponse = new SampleResponse()
+    def "A new AccountEntity is created"() {
         when:
-        def accountEntity = new AccountEntity("email", "username", "password", "firstName", "lastName");
+        accountEntity = new AccountEntity("email", "username", "password", "firstName", "lastName")
         then:
         accountEntity.email == "email"
         accountEntity.firstName == "firstName"
@@ -16,11 +28,27 @@ class ExampleSpecification extends Specification {
         accountEntity.password == "password"
         accountEntity.username == "username"
     }
-    def "A new IngredientEntity is added"() {
+    def "AccountEntity is added to database"() {
+        when:
+        accountEntity = new AccountEntity("email", "username", "password", "firstName", "lastName")
+        accountEntityResponse = accountController.registerAccount(accountEntity)
+        then:
+        Scanner sc = new Scanner(accountEntityResponse.toString())
+        sc.next() == "<201"
+    }
+    def "A new IngredientEntity is created"() {
         when:
         def ingredientEntity = new IngredientEntity("name", 100)
         then:
         ingredientEntity.name == "name"
         ingredientEntity.calories == 100
+    }
+    def "IngredientEntity is added to database"() {
+        when:
+        ingredientEntity = new IngredientEntity("name", 100)
+        ingredientEntityResponse = ingredientController.createIngredient(ingredientEntity)
+        then:
+        Scanner sc = new Scanner(ingredientEntityResponse.toString())
+        sc.next() == "<201"
     }
 }
--- src/test/groovy/com/nutrinav/NutrinavApplicationTests.groovy
+++ src/test/groovy/com/nutrinav/NutrinavApplicationTests.groovy
@@ -3,6 +3,7 @@ package com.nutrinav
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
+
 @SpringBootTest
 class NutrinavApplicationTests {
 
